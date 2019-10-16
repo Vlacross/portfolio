29bd2527ef3984e421253c3c8b50b2aa998ee176
Tim Vlasuk <timvlasuk@gmail.com>
2019-10-15 22:25:50 -0700

    added tab trap to keep tab focus on mobile menu; added auto focus to menu close button;

--- interactions.js
+++ interactions.js
@@ -7,8 +7,17 @@ const patties = document.querySelector('.burger-patty');
 const m1 = document.querySelector('.patty-one');
 const m2 = document.querySelector('.patty-two');
 
-let prevPos = 0;
+let navInputs = document.querySelector('.navDiv').querySelectorAll('a[href]:not([disabled]), \
+    button:not([disabled]), \
+    textarea:not([disabled]), \
+    input[type="text"]:not([disabled]), \
+    input[type="email"]:not([disabled]), \
+    input[type="textarea"]:not([disabled]), \
+    input[type="reset"]:not([disabled]), \
+    input[type="submit"]:not([disabled]), \
+    input[type="button"]:not([disabled])')
 
+let prevPos = 0;
 
 let mobileView = () => {
   /*Downside with this is that is closes menu view when resize occurs while menu is open */
@@ -19,7 +28,12 @@ let mobileView = () => {
     }
 };
 
+let navKeys = new navKeyHandle;
+
 menuOpen = () => {
+  document.getElementById('navStart').focus()
+  navKeys.tabOn(navInputs)
+  navKeys.escOn(menuClose)
   menuLink.forEach(link => link.addEventListener('click', function links(e, link) {
     menuClose()
     /*Works fine, though builds up listeners when menu is opened and closed(doesn't seem to 
@@ -69,6 +83,50 @@ function handleScroll() {
   prevPos = window.scrollY;
   }
 
+}
+
+
+function navKeyHandle() {
+  let tabsListeners;
+  let escListeners;
+
+  this.tabTrap = (e, ins) => {
+    if (e.code === "Tab") {
+      if(e.shiftKey) {
+        if (document.activeElement === ins[0]) {
+          ins[ins.length-1].focus()
+          e.preventDefault();
+        }
+      }
+       else {
+        if (document.activeElement === ins[ins.length-1]) {
+          ins[0].focus();
+          e.preventDefault();
+        } 
+      }
+    }
+  }
+
+  this.tabOn = (navInputs) => {
+    if(!tabsListeners) {
+      tabsListeners = true;
+      navBar.addEventListener('keydown', (e) => this.tabTrap(e, navInputs));
+    }
+  }
+
+  this.esc = (e, cb) => {
+    if(e.code === 'Escape') {
+      return cb()
+    }
+  }
+
+  this.escOn = (menuClose) => {
+    if(!escListeners) {
+      escListeners = true;
+      navBar.addEventListener('keydown', (e) => this.esc(e, menuClose))
+    }
+    
+  }
 }
 
 
